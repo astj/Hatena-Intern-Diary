b2a366975b33677797aa2925c34937ccf49c6c20
Asato Wakisaka <caaov627@hcn.zaq.ne.jp>
2013-08-16 23:25:37 +0900

    bookmarkからコードをコピペしてテスト時にセッションにログイン状況を保管できるようにした

--- t/lib/Test/Intern/Diary/Mechanize.pm
+++ t/lib/Test/Intern/Diary/Mechanize.pm
@@ -13,6 +13,8 @@ our @EXPORT = qw(create_mech);
 
 use Intern::Diary;
 
+my $app = Intern::Diary->as_psgi;
+
 sub create_mech (;%) {
     return __PACKAGE__->new(@_);
 }
@@ -20,8 +22,19 @@ sub create_mech (;%) {
 sub new {
     my ($class, %opts) = @_;
 
+    my $user = delete $opts{user};
+
+    my $user_mw = sub {
+        my $app = shift;
+        sub {
+            my $env = shift;
+            $env->{'psgix.session'}->{current_user_name} = $user ? $user->name : '';
+            $app->($env);
+        };
+    };
+
     my $self = $class->SUPER::new(
-        app     => Intern::Diary->as_psgi,
+        app     => $user_mw->($app),
         %opts,
     );
 
